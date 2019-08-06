ac8c9b3470cccc72dfeb36fa360a63a1473c0d8c
Yuxi <yuxi.liu@temasys.com.sg>
2019-08-06 13:11:59 +0800

    Change third party frameworks

--- frameworks/SocketIO.framework/Headers/SocketIO.h
+++ frameworks/SocketIO.framework/Headers/SocketIO.h
@@ -1,19 +1,19 @@
 //
-//  SocketIO-Mac.h
-//  SocketIO-Mac
+//  SocketIO-iOS.h
+//  SocketIO-iOS
 //
 //  Created by Nacho Soto on 7/11/15.
 //
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-//! Project version number for SocketIO-Mac.
-FOUNDATION_EXPORT double SocketIO_MacVersionNumber;
+//! Project version number for SocketIO-iOS.
+FOUNDATION_EXPORT double SocketIO_iOSVersionNumber;
 
-//! Project version string for SocketIO-Mac.
-FOUNDATION_EXPORT const unsigned char SocketIO_MacVersionString[];
+//! Project version string for SocketIO-iOS.
+FOUNDATION_EXPORT const unsigned char SocketIO_iOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SocketIO_Mac/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SocketIO_iOS/PublicHeader.h>
 
 
